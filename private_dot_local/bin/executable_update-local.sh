65ba54cec7d42458f84a1eaffbef5b52d88634de
symphonic-navigator <x@example.com>
2026-01-11 22:42:10 +0100

    added end-4 updates in the cleanest possible way

--- private_dot_local/bin/executable_update-local.sh
+++ private_dot_local/bin/executable_update-local.sh
@@ -12,9 +12,16 @@ echo "🌐 package update..."
 sudo pacman -Syu --noconfirm || true
 yay -Syu --noconfirm || true
 
+# --- update end-4 dotfiles ---
+echo "🖥️ updating end-4 dotfiles..."
+pushd ~/dots-hyprland
+git stash
+git pull
+bash -c "UV_VENV_CLEAR=1 ~/dots-hyprland/setup install -f --skip-sysupdate --skip-allgreeting --skip-miscconf --skip-fish --clean"
+
 # --- update lazyvim ---
 bash -c "$scriptsDir/update-lazyvim.sh"
 
 # --- chezmoi update ---
 echo "🥐 updating chezmoi..."
-chezmoi update
+chezmoi update --force
